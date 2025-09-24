16774aeec7004632892538ce547c5c1aa3022248
Helpful Projects <helpfulprojectsgithub@gmail.com>
2025-09-24 14:08:52 +0300

    we have logging to the console!

--- VoxelEngine/src/Core/EntryPoint.h
+++ VoxelEngine/src/Core/EntryPoint.h
@@ -3,6 +3,9 @@
 #ifdef VE_PLATFORM_WINDOWS
 extern VoxelEngine::Application* VoxelEngine::CreateApplication();
 int main(int argc, char** argv) {
+	VoxelEngine::Log::Init();
+	VE_CORE_WARN("Initialized Log again!");
+	VE_INFO("Hello again!");
 	auto app = VoxelEngine::CreateApplication();
 	app->Run();
 	delete app;
--- VoxelEngine/src/VoxelEngine.h
+++ VoxelEngine/src/VoxelEngine.h
@@ -2,6 +2,7 @@
 //For use by Voxel Engine applications
 
 #include "Core/Application.h"
+#include "Core/Log.h"
 
 //ENTRY POINT
 #include "Core/EntryPoint.h"
