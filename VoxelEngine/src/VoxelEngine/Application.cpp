dd35e1ef4d6106c88a1b866e55c153656527ba2d
Helpful Projects <helpfulprojectsgithub@gmail.com>
2025-09-29 11:41:52 +0300

    abstract vertex buffer layout

--- VoxelEngine/src/VoxelEngine/Application.cpp
+++ VoxelEngine/src/VoxelEngine/Application.cpp
@@ -7,6 +7,23 @@
 #include "Input.h"
 namespace VoxelEngine {
 	Application* Application::s_Instance = nullptr;
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type) {
+		case ShaderDataType::Float:  return GL_FLOAT;
+		case ShaderDataType::Float2: return GL_FLOAT;
+		case ShaderDataType::Float3: return GL_FLOAT;
+		case ShaderDataType::Float4: return GL_FLOAT;
+		case ShaderDataType::Mat3:   return GL_FLOAT;
+		case ShaderDataType::Mat4:   return GL_FLOAT;
+		case ShaderDataType::Int:    return GL_INT;
+		case ShaderDataType::Int2:   return GL_INT;
+		case ShaderDataType::Int3:   return GL_INT;
+		case ShaderDataType::Int4:   return GL_INT;
+		case ShaderDataType::Bool:   return GL_BOOL;
+		}
+		VE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 	Application::Application()
 	{
 		VE_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -19,16 +36,34 @@ namespace VoxelEngine {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f,
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.2f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"},
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 		uint32_t indices[3] = { 0,1,2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -37,9 +72,13 @@ namespace VoxelEngine {
 			#version 330 core
 
 			layout(location = 0) in vec4 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			out vec4 v_Color;
 
 			void main()
 			{
+				v_Color = a_Color;
 				gl_Position = a_Position;
 			}
 		)";
@@ -47,10 +86,11 @@ namespace VoxelEngine {
 			#version 330 core
 
 			out vec4 color;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(0.8, 0.2, 0.3, 1.0);
+				color = v_Color;
 			}
 		)";
 
