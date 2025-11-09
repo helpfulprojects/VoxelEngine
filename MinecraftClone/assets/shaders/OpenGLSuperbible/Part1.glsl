054914603190511a229545ad05a5a5aa8d37788e
Helpful Projects <helpfulprojectsgithub@gmail.com>
2025-11-09 22:00:49 +0200

    tessellation shader intro

--- MinecraftClone/assets/shaders/OpenGLSuperbible/Part1.glsl
+++ MinecraftClone/assets/shaders/OpenGLSuperbible/Part1.glsl
@@ -1,19 +1,57 @@
 #type vertex
 #version 450 core
+layout (location = 0) in vec4 offset;
+layout (location = 1) in vec4 color;
+
+out VS_OUT
+{
+	vec4 color;
+} vs_out;
 
 void main() { 
 
 	const vec4 vertices[3] = vec4[3]( vec4(0.25,-0.25,0.5,1.0),
 			vec4(-0.25,-0.25,0.5,1.0), vec4(0.25,0.25,0.5,1.0));
 
-	gl_Position = vertices[gl_VertexID];
+	gl_Position = vertices[gl_VertexID] + offset;
+	vs_out.color = color;
 
+}
+
+#type tessellationControl
+#version 450 core
+layout (vertices = 3) out;
+
+void main(){
+	if(gl_InvocationID == 0){
+		gl_TessLevelInner[0] = 5.0;
+		gl_TessLevelOuter[0] = 5.0;
+		gl_TessLevelOuter[1] = 5.0;
+		gl_TessLevelOuter[2] = 5.0;
+	}
+	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+}
+
+#type tesselationEvaluation
+#version 450 core
+layout (triangles, equal_spacing, cw) in;
+
+void main(){
+	gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position+
+		gl_TessCoord.y * gl_in[1].gl_Position+
+		gl_TessCoord.z * gl_in[2].gl_Position);
 }
 
 
 #type fragment
 #version 450 core 
 
-out vec4 FragColor;
+// in VS_OUT
+// {
+// 	vec4 color;
+// } fs_in;
+out vec4 color;
 
-void main() { FragColor = vec4(0.0,0.8,1.0,1.0); } 
+void main() { 
+	color = vec4(0.0f, 0.8f, 1.0f, 1.0f);
+}
--- MinecraftClone/src/OpenGLSuperbible/Part1.h
+++ MinecraftClone/src/OpenGLSuperbible/Part1.h
@@ -7,21 +7,29 @@ public:
   ~Part1() {};
   void OnAttach() override {
     VoxelEngine::Application &application = VoxelEngine::Application::Get();
-    // application.GetWindow().SetMaximized(true);
+    application.GetWindow().SetMaximized(true);
     m_ShaderLibrary.Load("assets/shaders/OpenGLSuperbible/Part1.glsl");
     uint32_t vao;
     glCreateVertexArrays(1, &vao);
     glBindVertexArray(vao);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glEnableVertexArrayAttrib(vao, 0);
   };
   void OnUpdate(VoxelEngine::Timestep ts) override {
     VoxelEngine::Application &application = VoxelEngine::Application::Get();
     float currentTime = application.GetWindow().GetTime();
     GLfloat red[] = {0.0f, 0.2f, 0.0f, 1.0f};
+    GLfloat attrib[] = {(float)sin(currentTime) * 0.5f,
+                        (float)cos(currentTime) * 0.6f, 0.0f, 0.0f};
+    glVertexAttrib4fv(0, attrib);
+    GLfloat color[] = {0.0f, 0.8f, 1.0f, 1.0f};
+    glVertexAttrib4fv(1, color);
 
     auto shader = m_ShaderLibrary.Get("Part1");
     glClearBufferfv(GL_COLOR, 0, red);
     shader->Bind();
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_PATCHES, 0, 3);
   };
   void OnEvent(VoxelEngine::Event &event) override {
     VoxelEngine::EventDispatcher dispatcher(event);
